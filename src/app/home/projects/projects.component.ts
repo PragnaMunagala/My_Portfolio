e16611e2b24da3030b66fbed584036e580515f38
pragna <pmunagal@asu.edu>
2018-03-14 15:26:38 -0700

    projects component

--- src/app/home/projects/projects.component.ts
+++ src/app/home/projects/projects.component.ts
@@ -15,7 +15,77 @@ export class ProjectsComponent implements OnInit {
           desc: ['test', 'test']
         },
         {
-          title: 'Image classification using KNN',
+          title: 'Image classification using K-Nearest Neighbor',
+          desc: ['test', 'test']
+        }
+      ],
+      show: false
+    },
+    {
+      category: 'Android Programming',
+      project: [
+        {
+          title: 'BraiNet Application',
+          desc: ['test', 'test']
+        },
+        {
+          title: 'Health Monitor Application',
+          desc: ['test', 'test']
+        }
+      ],
+      show: false
+    },
+    {
+      category: 'Software Security',
+      project: [
+        {
+          title: 'Secure Banking Application',
+          desc: ['test', 'test']
+        }
+      ],
+      show: false
+    },
+    {
+      category: 'Linux Kernel - Device Driver development',
+      project: [
+        {
+          title: 'I2C',
+          desc: ['test', 'test']
+        },
+        {
+          title: 'SPI',
+          desc: ['test', 'test']
+        },
+        {
+          title: 'Real time',
+          desc: ['test', 'test']
+        }
+      ],
+      show: false
+    },
+    {
+      category: 'Computer Architecture',
+      project: [
+        {
+          title: 'Cache',
+          desc: ['test', 'test']
+        },
+        {
+          title: 'Jump',
+          desc: ['test', 'test']
+        }
+      ],
+      show: false
+    },
+    {
+      category: 'Digital Image and Video Processing',
+      project: [
+        {
+          title: 'Image Filtering',
+          desc: ['test', 'test']
+        },
+        {
+          title: 'Android Application - Image',
           desc: ['test', 'test']
         }
       ],
