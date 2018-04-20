b57a29ff4c23fb8ef4e36746f11874d60188d99a
pragnamungala <pmunagal@asu.edu>
2018-04-20 11:32:04 -0700

    resume update

--- src/app/home/skills/skills.component.ts
+++ src/app/home/skills/skills.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
   skillset = [
-    {name: 'Front-end technologies', tech: ['HTML', ' CSS', ' JavaSript', ' JQuery', ' Angular 2+', ' AngularJS', ' Bootstrap', ' PHP', ' Laravel', ' Sightly']},
+    {name: 'Front-end technologies', tech: ['HTML', ' CSS', ' JavaSript', ' JQuery', ' Angular 2+', ' AngularJS', ' React JS', ' Bootstrap', ' PHP', ' Laravel', ' Sightly']},
     {name: 'Programming Languages', tech: ['C', ' Python', ' C++', ' Java', ' Android', ' OpenCV', ' Adobe Experience Manager(AEM)']},
     {name: 'Other Development', tech: ['Machine Learning', ' Linux Kernel', ' Bash Scripting', ' Device Driver Implementation']},
     {name: 'Operating Systems', tech: ['Windows', ' Linux', ' Zephyr']},
